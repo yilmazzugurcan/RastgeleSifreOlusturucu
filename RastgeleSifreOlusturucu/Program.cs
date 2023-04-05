90e1b561ced9eddf4c80609ffd7123d15b20d3fd
yilma <yilma@DESKTOP-U1LH654>
2023-04-05 23:14:26 +0300

    Add project files.

---
+++ RastgeleSifreOlusturucu/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace RastgeleSifreOlusturucu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var random = new Random();
+            var result = new string(
+                Enumerable.Repeat(chars, 15) // 8 karakterli bir şifre oluşturmak için chars'ı 8 kere tekrarla
+                          .Select(s => s[random.Next(s.Length)]) // chars'ın rastgele bir karakterini seç
+                          .ToArray());
+
+            Console.WriteLine("Rastgele şifre: " + result);
+
+            var resultWithDigits = new string(
+                Enumerable.Repeat(chars + "0123456789", 20) // 10 karakterli bir şifre oluşturmak için chars'ı 8 kere tekrarla ve rakamları da ekleyin
+                          .Select(s => s[random.Next(s.Length)]) // chars ve rakamların rastgele bir karakterini seç
+                          .ToArray());
+
+            Console.WriteLine("Rastgele şifre (rakamlarla birlikte): " + resultWithDigits);
+        }
+    }
+}
